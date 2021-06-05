44ad9902ca9d17ee8b038366dfa53a5fb683cabe
Namdak Tonpa <maxim@synrc.com>
2021-06-05 18:10:11 +0300

    line up

--- eval.ml
+++ eval.ml
@@ -48,35 +48,34 @@ let rec eval (e : exp) (rho : rho) =
     Printf.printf "EVAL: %s\n" (showExp e); flush_all ()
   end else ();
   match e with
-  | ESet u -> VSet u
+  | ESet u           -> VSet u
   | ELam ((p, a), b) -> VLam (eval a rho, (p, b, rho))
   | EPi  ((p, a), b) -> VPi  (eval a rho, (p, b, rho))
   | ESig ((p, a), b) -> VSig (eval a rho, (p, b, rho))
-  | EFst e -> vfst (eval e rho)
-  | ESnd e -> vsnd (eval e rho)
-  | EApp (f, x) -> app (eval f rho, eval x rho)
-  | EVar x -> getRho rho x
-  | EPair (e1, e2) -> VPair (eval e1 rho, eval e2 rho)
-  | EHole -> VNt NHole
-  | EUndef -> VNt NUndef
+  | EFst e           -> vfst (eval e rho)
+  | ESnd e           -> vsnd (eval e rho)
+  | EApp (f, x)      -> app (eval f rho, eval x rho)
+  | EVar x           -> getRho rho x
+  | EPair (e1, e2)   -> VPair (eval e1 rho, eval e2 rho)
+  | EHole            -> VNt NHole
+  | EUndef           -> VNt NUndef
 and app : value * value -> value = function
   | VLam (_, f), v -> closByVal f v
   | VNt k, m       -> VNt (NApp (k, m))
   | x, y           -> raise (InvalidApplication (x, y))
 and closByVal (x : clos) (v : value) =
   let (p, e, rho) = x in if !Prefs.trace then begin
-    Printf.printf "CLOSBYVAL: (%s)(%s := %s)\n"
-      (showExp e) (showName p) (showValue v);
+    Printf.printf "CLOSBYVAL: (%s)(%s := %s)\n" (showExp e) (showName p) (showValue v);
     flush_all ()
   end else (); eval e (upVar rho p v)
 and getRho rho x =
   match Env.find_opt x rho with
   | Some (Value v) -> v
-  | Some (Exp e) -> eval e rho
-  | None -> raise (VariableNotFound x)
+  | Some (Exp e)   -> eval e rho
+  | None           -> raise (VariableNotFound x)
 
 let gen : int ref = ref 0
-let pat : name -> name = (gen := !gen + 1); function
+let pat : name  -> name = (gen := !gen + 1); function
   | No          -> No
   | Name (p, _) -> Name (p, !gen)
 
@@ -84,19 +83,13 @@ let var x  = VNt (NVar x)
 let genV n = var (pat n)
 
 let rec rbV : value -> exp = function
-  | VLam (t, g)    ->
-    let (p, _, _) = g in let q = pat p in
-    ELam ((q, rbV t), rbV (closByVal g (var q)))
-  | VPair (u, v)   -> EPair (rbV u, rbV v)
-  | VSet u         -> ESet u
-  | VPi (t, g)     ->
-    let (p, _, _) = g in let q = pat p in
-    EPi ((q, rbV t), rbV (closByVal g (var q)))
-  | VSig (t, g)    ->
-    let (p, _, _) = g in let q = pat p in
-    ESig ((q, rbV t), rbV (closByVal g (var q)))
-  | VNt l          -> rbN l
-and rbN : neut -> exp = function
+  | VLam (t, g)     -> let (p, _, _) = g in let q = pat p in ELam ((q, rbV t), rbV (closByVal g (var q)))
+  | VPair (u, v)    -> EPair (rbV u, rbV v)
+  | VSet u          -> ESet u
+  | VPi (t, g)      -> let (p, _, _) = g in let q = pat p in EPi ((q, rbV t), rbV (closByVal g (var q)))
+  | VSig (t, g)     -> let (p, _, _) = g in let q = pat p in ESig ((q, rbV t), rbV (closByVal g (var q)))
+  | VNt l           -> rbN l
+and rbN : neut  -> exp = function
   | NVar s      -> EVar s
   | NApp (k, m) -> EApp (rbN k, rbV m)
   | NFst k      -> EFst (rbN k)
@@ -106,29 +99,22 @@ and rbN : neut -> exp = function
 
 let rec conv v1 v2 : bool =
   if !Prefs.trace then begin
-    Printf.printf "CONV: %s = %s\n"
-      (showValue v1) (showValue v2);
+    Printf.printf "CONV: %s = %s\n" (showValue v1) (showValue v2);
     flush_all ()
   end else ();
   match v1, v2 with
-  | VSet u, VSet v -> ieq u v
-  | VNt x, VNt y -> convNeut x y
+  | VSet u, VSet v             -> ieq u v
+  | VNt x, VNt y               -> convNeut x y
   | VPair (a, b), VPair (c, d) -> conv a c && conv b d
-  | VPair (a, b), v -> conv a (vfst v) && conv b (vsnd v)
-  | v, VPair (a, b) -> conv (vfst v) a && conv (vsnd v) b
+  | VPair (a, b), v            -> conv a (vfst v) && conv b (vsnd v)
+  | v, VPair (a, b)            -> conv (vfst v) a && conv (vsnd v) b
   | VPi (a, g), VPi (b, h)
   | VSig (a, g), VSig (b, h)
-  | VLam (a, g), VLam (b, h) ->
-    let (p, _, _) = g in let p' = genV p in
-
-    let (_, e1, _) = g in let (_, e2, _) = h in
-    conv a b && (e1 = e2 || conv (closByVal g p') (closByVal h p'))
-  | VLam (a, g), b ->
-    let (p, _, _) = g in let p' = genV p in
-    conv (closByVal g p') (app (b, p'))
-  | b, VLam (a, g) ->
-    let (p, _, _) = g in let p' = genV p in
-    conv (app (b, p')) (closByVal g p')
+  | VLam (a, g), VLam (b, h)   ->
+      let (p, e1, _) = g in let (_, e2, _) = h in
+      conv a b && (e1 = e2 || conv (closByVal g (genV p)) (closByVal h (genV p)))
+  | VLam (a, g), b -> let (p, _, _) = g in let p' = genV p in conv (closByVal g p') (app (b, p'))
+  | b, VLam (a, g) -> let (p, _, _) = g in let p' = genV p in conv (app (b, p')) (closByVal g p')
   | _, _ -> false
 and convNeut n1 n2 : bool =
   match n1, n2 with
@@ -140,8 +126,7 @@ and convNeut n1 n2 : bool =
 
 let eqNf v1 v2 : unit =
   if !Prefs.trace then begin
-    Printf.printf "EQNF: %s = %s\n"
-      (showValue v1) (showValue v2);
+    Printf.printf "EQNF: %s = %s\n" (showValue v1) (showValue v2);
     flush_all ()
   end else ();
   if conv v1 v2 then ()

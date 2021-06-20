80aeabc523c72a2b6d0491dd829261d646e774fd
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-06-20 12:46:17 +0700

    description of algorithm

--- formula.ml
+++ formula.ml
@@ -25,6 +25,10 @@ let rec negNeut : neut -> neut = function
   | NOr (f, g)  -> andNeut (negNeut f, negNeut g)
   | k           -> raise (InvalidFormulaNeg (VNt k))
 
+(* Arbitrary formula φ after calling andFormula/orFormula/negFormula
+   will have form (α₁ ∧ ... ∧ αₙ) ∨ ... ∨ (β₁ ∧ ... ∧ βₘ),
+   where “∧” and “∨” are right-associative,
+   and each αᵢ/βⱼ has form “γ” or “−γ” for some variable “γ”. *)
 let andFormula a b =
   match a, b with
   | VNt u, VNt v -> VNt (andNeut (u, v))
@@ -48,6 +52,7 @@ end
 
 module Atoms = Set.Make(Atom)
 
+(* extAnd converts (α₁ ∧ ... ∧ αₙ) into set of names equipped with sign. *)
 let rec extAnd : neut -> Atoms.t = function
   | NVar x               -> Atoms.singleton (x, true)
   | NNeg (NVar x)        -> Atoms.singleton (x, false)
@@ -56,20 +61,32 @@ let rec extAnd : neut -> Atoms.t = function
   | NAnd (x, y)          -> Atoms.union (extAnd x) (extAnd y)
   | k -> failwith (Printf.sprintf "“%s” expected to be conjuction (should never happen)" (showNeut k))
 
+(* extOr converts (α₁ ∧ ... ∧ αₙ) ∨ ... ∨ (β₁ ∧ ... ∧ βₘ)
+   into list of extAnd results. *)
 type disjunction = Atoms.t list
 let rec extOr : neut -> disjunction = function
   | NOr (x, y) -> List.rev_append (extOr x) (extOr y)
   | k          -> [extAnd k]
 
+(* uniq removes all conjunctions that are subset of another,
+   i. e. xy ∨ x = (x ∧ y) ∨ (x ∧ 1) = x ∧ (y ∨ 1) = x ∧ 1 = x.
+   It does not remove conjuctions like (x ∧ −x), because algebra of interval
+   is not boolean, it is De Morgan algebra: distributive lattice with De Morgan laws.
+   https://ncatlab.org/nlab/show/De+Morgan+algebra *)
 let uniq f =
   let super x y = not (Atoms.equal x y) && Atoms.subset y x in
   List.filter (fun x -> not (List.exists (super x) f)) f
 
+(* orSubset checks that all conjunctions from xs present in ys. *)
 let orSubset xs ys =
-  List.for_all (fun y -> List.exists (Atoms.equal y) xs) ys
+  List.for_all (fun x -> List.exists (Atoms.equal x) ys) xs
 
+(* orEq checks equivalence of two formulas
+   of the form (α₁ ∧ ... ∧ αₙ) ∨ ... ∨ (β₁ ∧ ... ∧ βₘ) *)
 let orEq f g =
   let f' = uniq (extOr f) in let g' = uniq (extOr g) in
   orSubset f' g' && orSubset g' f'
 
+(* andEq check equivalence of two formulas
+   of the form (α₁ ∧ ... ∧ αₙ) *)
 let andEq f g = Atoms.equal (extAnd f) (extAnd g)

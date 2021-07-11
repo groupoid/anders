92f11f6c080ffa75b62a9f4177af48eb48f1695e
5HT <maxim@synrc.com>
2021-07-11 04:11:49 +0300

    cleanup matches in expr

--- expr.ml
+++ expr.ml
@@ -33,8 +33,11 @@ type value =
   | VSub of value * value * value | VInc of value | VOuc of value
 
 and clos = name * exp * ctx
+
 and term = Exp of exp | Value of value
+
 and record = scope * term * term
+
 and ctx = record Env.t
 
 (* Implementation *)
@@ -52,9 +55,7 @@ let name x = Name (x, 0)
 let decl x = EVar (name x)
 
 let isGlobal : record -> bool = function Global, _, _ -> false | Local, _, _ -> true
-
 let freshVar ns n = match Env.find_opt n ns with Some x -> x | None -> n
-
 let mapFace fn phi = Env.fold (fun p d -> Env.add (fn p) d) phi Env.empty
 let freshFace ns = mapFace (freshVar ns)
 
@@ -65,6 +66,7 @@ let face i n d = match i with
 let rec telescope (ctor : name -> exp -> exp -> exp) (e : exp) : tele list -> exp = function
   | (p, a) :: xs -> ctor p a (telescope ctor e xs)
   | [] -> e
+
 let rec pLam e : name list -> exp = function [] -> e | x :: xs -> EPLam (ELam (EI, (x, pLam e xs)))
 
 let getDigit x = Char.chr (x + 0x80) |> Printf.sprintf "\xE2\x82%c"
@@ -80,17 +82,13 @@ let showDir : dir -> string = function | Zero -> !zeroPrim | One -> !onePrim
 let showAtom (p, d) = Printf.sprintf "(%s = %s)" (showName p) (showDir d)
 let showFace xs = Env.bindings xs |> List.map showAtom |> String.concat " "
 let showSystem show = function
-  | Split xs -> List.map (fun (x, e) -> Printf.sprintf "%s → %s" (showFace x) (show e)) xs
-                |> String.concat ", "
+  | Split xs -> List.map (fun (x, e) -> Printf.sprintf "%s → %s" (showFace x) (show e)) xs |> String.concat ", "
   | Const x -> Printf.sprintf "_ → %s" (show x)
 
 let rec showExp : exp -> string = function
   | EKan n -> "U" ^ showLevel n
   | ELam (a, (p, b)) -> Printf.sprintf "λ %s, %s" (showTele p a) (showExp b)
-  | EPi (a, (p, b)) -> begin match p with
-    | Irrefutable -> Printf.sprintf "(%s → %s)" (showExp a) (showExp b)
-    | _           -> Printf.sprintf "Π %s, %s" (showTele p a) (showExp b)
-  end
+  | EPi (a, (p, b)) -> showPiExp a p b
   | ESig (a, (p, b)) -> Printf.sprintf "Σ %s, %s" (showTele p a) (showExp b)
   | EPair (fst, snd) -> Printf.sprintf "(%s, %s)" (showExp fst) (showExp snd)
   | EFst exp -> showExp exp ^ ".1"
@@ -117,15 +115,17 @@ let rec showExp : exp -> string = function
   | ENeg a -> Printf.sprintf "-%s" (showExp a)
   | EInc e -> Printf.sprintf "(inc %s)" (showExp e)
   | EOuc e -> Printf.sprintf "(ouc %s)" (showExp e)
+
 and showTele p x = Printf.sprintf "(%s : %s)" (showName p) (showExp x)
+
+and showPiExp a p b = match p with
+  | Irrefutable -> Printf.sprintf "(%s → %s)" (showExp a) (showExp b)
+  | _           -> Printf.sprintf "Π %s, %s" (showTele p a) (showExp b)
 
 let rec showValue : value -> string = function
   | VKan n -> "U" ^ showLevel n
   | VLam (x, (p, e, rho)) -> Printf.sprintf "λ %s, %s" (showTele p x rho) (showExp e)
-  | VPi (x, (p, e, rho)) -> begin match p with
-    | Irrefutable -> Printf.sprintf "(%s → %s)" (showValue x) (showExp e)
-    | _           -> Printf.sprintf "Π %s, %s" (showTele p x rho) (showExp e)
-  end
+  | VPi (x, (p, e, rho)) -> showPi x p e rho
   | VSig (x, (p, e, rho)) -> Printf.sprintf "Σ %s, %s" (showTele p x rho) (showExp e)
   | VPair (fst, snd) -> Printf.sprintf "(%s, %s)" (showValue fst) (showValue snd)
   | VFst v -> showValue v ^ ".1"
@@ -140,17 +140,11 @@ let rec showValue : value -> string = function
   | VJ v -> Printf.sprintf "idJ %s" (showValue v)
   | VTransp (p, i) -> Printf.sprintf "transp %s %s" (showValue p) (showValue i)
   | VHComp v -> Printf.sprintf "hcomp %s" (showValue v)
-  | VPLam (VLam (_, (p, e, rho))) ->
-    if isRhoVisible rho then
-      Printf.sprintf "(<%s, %s> %s)" (showName p) (showRho rho) (showExp e)
-    else Printf.sprintf "(<%s> %s)" (showName p) (showExp e)
+  | VPLam (VLam (_, (p, e, rho))) -> showLam p e rho
   | VPLam _ -> failwith "showExp: unreachable code was reached"
   | VAppFormula (f, x) -> Printf.sprintf "(%s @ %s)" (showValue f) (showValue x)
   | VPartial v -> Printf.sprintf "Partial %s" (showValue v)
-  | VSystem (x, rho) ->
-    if isRhoVisible rho then
-      Printf.sprintf "[%s, %s]" (showSystem showExp x) (showRho rho)
-    else Printf.sprintf "[%s]" (showSystem showExp x)
+  | VSystem (x, rho) -> showSystemV x rho
   | VSub (a, i, u) -> Printf.sprintf "%s[%s ↦ %s]" (showValue a) (showValue i) (showValue u)
   | VInc v -> Printf.sprintf "(inc %s)" (showValue v)
   | VOuc v -> Printf.sprintf "(ouc %s)" (showValue v)
@@ -158,14 +152,31 @@ let rec showValue : value -> string = function
   | VAnd (a, b) -> Printf.sprintf "(%s ∧ %s)" (showValue a) (showValue b)
   | VOr (a, b) -> Printf.sprintf "(%s ∨ %s)" (showValue a) (showValue b)
   | VNeg a -> Printf.sprintf "-%s" (showValue a)
-and showTermBind : name * record -> string option = function
-  | p, (Local, _, t) -> Some (Printf.sprintf "%s := %s" (showName p) (showTerm t))
-  | _, _             -> None
-and isRhoVisible = Env.exists (fun _ -> isGlobal)
-and showRho ctx : string = Env.bindings ctx |> List.filter_map showTermBind |> String.concat ", "
+
 and showTele p x rho : string =
   if isRhoVisible rho then Printf.sprintf "(%s : %s, %s)" (showName p) (showValue x) (showRho rho)
   else Printf.sprintf "(%s : %s)" (showName p) (showValue x)
+
+and showLam p e rho =
+  if isRhoVisible rho then Printf.sprintf "(<%s, %s> %s)" (showName p) (showRho rho) (showExp e)
+  else Printf.sprintf "(<%s> %s)" (showName p) (showExp e)
+
+and showSystemV x rho =
+  if isRhoVisible rho then Printf.sprintf "[%s, %s]" (showSystem showExp x) (showRho rho)
+  else Printf.sprintf "[%s]" (showSystem showExp x)
+
+and showTermBind : name * record -> string option = function
+  | p, (Local, _, t) -> Some (Printf.sprintf "%s := %s" (showName p) (showTerm t))
+  | _, _             -> None
+
+and showPi x p e rho = match p with
+  | Irrefutable -> Printf.sprintf "(%s → %s)" (showValue x) (showExp e)
+  | _           -> Printf.sprintf "Π %s, %s" (showTele p x rho) (showExp e)
+
+and isRhoVisible = Env.exists (fun _ -> isGlobal)
+
+and showRho ctx : string = Env.bindings ctx |> List.filter_map showTermBind |> String.concat ", "
+
 and showTerm : term -> string = function Exp e -> showExp e | Value v -> showValue v
 
 let showGamma (ctx : ctx) : string =

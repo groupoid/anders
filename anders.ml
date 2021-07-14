e3789d5c17825bd0a176c94a547c52d8ff087f17
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-07-14 14:41:02 +0700

    option for indices

--- anders.ml
+++ anders.ml
@@ -7,7 +7,7 @@ type cmdline =
   | Parse     of string
   | Cubicaltt of string
   | Prim      of string * string
-  | Repl | Help | Trace | Girard | Silent
+  | Repl | Help | Trace | Indices | Girard | Silent
 
 let help =
 "\n  invocation = anders | anders list
@@ -17,8 +17,9 @@ let help =
      command = check <filename>      | lex <filename>
              | parse <filename>      | prim primitive <name>
              | cubicaltt <filename>  | girard
-             | trace                 | silent
-             | repl                  | help "
+             | trace                 | indices
+             | silent                | repl
+             | help "
 
 let repl = ref false
 let cmd : cmdline -> unit = function
@@ -35,7 +36,8 @@ let cmd : cmdline -> unit = function
   | Help -> print_endline Repl.banner ; print_endline help
   | Repl -> repl := true
   | Silent -> Prefs.verbose := false
-  | Trace -> Prefs.trace := true
+  | Indices -> Prefs.indices := true
+  | Trace -> Prefs.indices := true; Prefs.trace := true
   | Girard -> Prefs.girard := true
 
 let rec parseArgs : string list -> cmdline list = function
@@ -45,11 +47,12 @@ let rec parseArgs : string list -> cmdline list = function
   | "lex"       :: filename :: rest -> Lex       filename :: parseArgs rest
   | "parse"     :: filename :: rest -> Parse     filename :: parseArgs rest
   | "cubicaltt" :: filename :: rest -> Cubicaltt filename :: parseArgs rest
-  | "help"      :: rest             -> Help   :: parseArgs rest
-  | "trace"     :: rest             -> Trace  :: parseArgs rest
-  | "silent"    :: rest             -> Silent :: parseArgs rest
-  | "girard"    :: rest             -> Girard :: parseArgs rest
-  | "repl"      :: rest             -> Repl   :: parseArgs rest
+  | "help"      :: rest             -> Help    :: parseArgs rest
+  | "trace"     :: rest             -> Trace   :: parseArgs rest
+  | "indices"   :: rest             -> Indices :: parseArgs rest
+  | "silent"    :: rest             -> Silent  :: parseArgs rest
+  | "girard"    :: rest             -> Girard  :: parseArgs rest
+  | "repl"      :: rest             -> Repl    :: parseArgs rest
   | x :: xs -> Printf.printf "Unknown command “%s”\n" x; parseArgs xs
 
 let defaults = function
--- ident.ml
+++ ident.ml
@@ -4,7 +4,7 @@ type name =
 
 let showName : name -> string = function
   | Irrefutable -> "_"
-  | Name (p, n) -> if !Prefs.trace then p ^ "#" ^ string_of_int n else p
+  | Name (p, n) -> if !Prefs.indices then p ^ "#" ^ string_of_int n else p
 
 module Name = struct
   type t = name
